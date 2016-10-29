4da6b50e8acd5c8075770c1f8a8a2585754656d4
shainaveloz <shaina.veloz@gmail.com>
2016-10-28 22:55:43 -0400

    contact testing

--- views/js/contact_me.js
+++ views/js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact.js",
+                url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
