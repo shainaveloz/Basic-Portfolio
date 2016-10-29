5197c998928a37001dd153368c740e59e0ad72d3
shainaveloz <shaina.veloz@gmail.com>
2016-10-28 20:40:56 -0400

    test

--- views/js/contact_me.js
+++ views/js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.js",
+                url: "././js/contact_me.js",
                 type: "POST",
                 data: {
                     name: name,
